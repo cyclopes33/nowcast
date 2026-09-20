631387b604a93c411b6c4668092b054ef45c4ff5
cyclopes33 <03ankitsaha@gmail.com>
2026-09-20 14:06:39 +0530

    Update website chnages

--- script.js
+++ script.js
@@ -58,3 +58,30 @@ setInterval(() => {
         `${value} <span>mm/hr</span>`;
 
 }, 4000);
+// ===============================
+// FAQ ACCORDION
+// ===============================
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        // Close other FAQ items
+        faqItems.forEach(otherItem => {
+
+            if (otherItem !== item) {
+                otherItem.classList.remove("active");
+            }
+
+        });
+
+        // Toggle current FAQ
+        item.classList.toggle("active");
+
+    });
+
+});
